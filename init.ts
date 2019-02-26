6223dc58b3e8751e859076c2016dd50bcb45175a
alexsey <Kiselev@L44-18.lab44.org>
2019-02-26 11:16:03 +0700

    dfdfdf

--- init.ts
+++ init.ts
@@ -2,3 +2,5 @@
 console.log('Testing app project-testing branch');
 console.log('Testing app project-testing branch2');
 console.log('Product assigned version v0.2');
+console.log('Product assigned version v0.3');
+
